ac3c18d6d27d475e75c3e9d5b72b6d7f4cb24c55
mravaloarison <100179286+mravaloarison@users.noreply.github.com>
2024-11-20 23:16:27 -0500

    resume

--- src/components/DownloadResume.js
+++ src/components/DownloadResume.js
@@ -5,7 +5,7 @@ import CloudDownloadOutlinedIcon from "@mui/icons-material/CloudDownloadOutlined
 export default function DownloadResume() {
 	const downloadResume = () => {
 		window.open(
-			"https://drive.google.com/file/d/1I0UHi9_8CpcIdoy62vHd-pyt_49gMhJT/view?usp=sharing",
+			"https://drive.google.com/file/d/131r91PeRhD40eNl6LwpilcLdLiBnen5j/view?usp=sharing",
 			"_blank"
 		);
 	};
