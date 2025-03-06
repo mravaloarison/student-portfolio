9cb44d9bc3f7e039ec37fb0a51e8641ccecfbf14
mravaloarison <100179286+mravaloarison@users.noreply.github.com>
2025-03-06 14:47:30 -0500

    fakedb added

---
+++ fakedb.ts
@@ -0,0 +1,43 @@
+export const SocialMediaLinks = [
+    {
+        icon: "https://img.icons8.com/?size=100&id=12160&format=png&color=000000",
+        link: "https://drive.google.com/file/d/1csURe3jL7UQ6vr_wbh4JWTry-sZfPkNQ/view?usp=sharing",
+        name: "Resume",
+    },
+    {
+        icon: "https://img.icons8.com/?size=100&id=13930&format=png&color=000000",
+        link: "https://www.linkedin.com/in/mravaloarison/",
+        name: "LinkedIn",
+    },
+    {
+        icon: "https://img.icons8.com/?size=100&id=12599&format=png&color=000000",
+        link: "https://github.com/mravaloarison",
+        name: "GitHub",
+    },
+    {
+        icon: "https://img.icons8.com/?size=100&id=Xy10Jcu1L2Su&format=png&color=000000",
+        link: "https://www.instagram.com/batman_rvl/?hl=en",
+        name: "Instagram",
+    }
+];
+
+export const projects = [
+    {
+        title: "Green",
+        description:
+            "Green is an app built to reduce traffic emissions by helping drivers find routes with minimal stops as possible. It analyzes current traffic for a route and dynamically recommends routes that avoid traffic hotspots, ensuring drivers spend less time in traffic even though, now the travel time has increased.",
+        year: "November 2023",
+    },
+    {
+        title: "CatCulus",
+        description:
+            "A simple Pygame that leverages Machine Learning and Artificial Intelligence to save a cat by solving math problems. The design and game logic were primarily inspired by reverse engineering the Google Cat Academy Third edition.",
+        year: "October 2025",
+    },
+    {
+        title: "Flappy x ML",
+        description:
+            "Flappy Bird using face detection to control the bird.",
+        year: "September 2025",
+    },
+];
